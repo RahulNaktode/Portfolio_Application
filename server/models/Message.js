c45b69c5640795795e22f71be71a2ef7c66f0e1a
RahulNaktode <rahulnaktode04@gmail.com>
2026-07-25 21:25:47 +0530

    add models

---
+++ server/models/Message.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const messageSchema = new Schema({
+  name: { 
+    type: String, 
+    required: true 
+},
+  email: { 
+    type: String, 
+    required: true 
+},
+  message: { 
+    type: String, 
+    required: true 
+},
+  isRead: { 
+    type: Boolean, 
+    default: false 
+}
+}, { timestamps: true });
+
+const Message = model('Message', messageSchema);
+export default Message;
---
+++ server/models/Project.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const ProjectSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tags: [String],
+    imageUrl: {
+        type: String
+    },
+    demoLink: {
+        type: String
+    },
+    githubLink: {
+        type: String
+    },
+    featured: {
+        type: Boolean, default: false
+    }
+}, { timestamps: true });
+
+const Project = model("Project", ProjectSchema);
+
+export default Project;

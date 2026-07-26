f460210e9ed62e3d8dbb7629bb35951699f54112
RahulNaktode <rahulnaktode04@gmail.com>
2026-07-26 17:55:38 +0530

    add middelwar and health controller

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connentDB from "./db.js";
+import { getHome, getHealth } from "./controllers/health.js";
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
+
+app.get("/", getHome);
+app.get("/health", getHealth);
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on PORT ${PORT}`);
